f31cf1dd3e83475507ef855e49faabca00ee8b01
Han <han.kyaw@ascenderhcm.com>
2021-10-14 16:54:46 +1100

    add angular flex lib and clean up material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './layout/layout/layout.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { AppMaterialModule } from './app-material/app-material.module';
+
 import { AppConfig, APP_CONFIG } from './app.config';
 import { AppRoutingModule } from './app.routing.module';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [AppComponent, LayoutComponent, HomeComponent],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        LayoutModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-        AppRoutingModule,
-    ],
+    imports: [BrowserModule, BrowserAnimationsModule, LayoutModule, AppMaterialModule, AppRoutingModule],
     providers: [
         {
             provide: APP_CONFIG,
